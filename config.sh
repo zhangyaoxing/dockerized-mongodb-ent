342179249f91fa9623532e0a649732c8b55d8575
Yaoxing <yaoxing.zhang@gmail.com>
2021-02-05 14:46:15 +0800

    * fix a wrong param name

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DOCKER_USERNAME=emsd
+export DOCKER_USERNAME=yaoxing
 export MONGODB_VERSION=4.4
 export _HTTPS_PROXY="" # specify https proxy if available
 export _HTTP_PROXY=$_HTTPS_PROXY # specify http proxy if available
